1d69304ff7fd3f2f10fee3333a5cd5245045ff80
Vitor Mateus <vitor7jan@gmail.com>
2023-04-08 17:20:18 -0300

    text-editor with thread in busy waiting

--- text-editor.c
+++ text-editor.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
-#include <signal.h>
-#include <unistd.h>
 
 char text[16 * 4] = "";
 
@@ -19,19 +17,22 @@ void *input(void *)
     }
 }
 
-void save(int)
+void *save(void *)
 {
-    signal(SIGALRM, save);
-    printf("\nsaved: %s\n", text);
-    strcpy(text, "");
-    alarm(3);
+    while (1) /* espera ocupada */
+    {
+        if (strlen(text) == 0)
+            continue;
+        printf("\nsaved: %s\n", text);
+        strcpy(text, "");
+    }
 }
 
 int main(void)
 {
     pthread_t input_thread, save_thread;
     pthread_create(&input_thread, 0, input, 0);
-    save(0);
+    pthread_create(&save_thread, 0, save, 0);
     pthread_join(input_thread, 0);
     return 0;
 }
