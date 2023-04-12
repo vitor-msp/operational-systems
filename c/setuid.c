0d6608aea73bbf0f8ed7a9805afb6d32d480bc13
Vitor Mateus <vitor7jan@gmail.com>
2023-04-11 23:15:34 -0300

    added setuid

---
+++ c/setuid.c
@@ -0,0 +1,48 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+#define ROOT 0
+
+// comandos para o executavel deste arquivo:
+// chown root:root
+// testar execucao sem ser como root
+// chmod u+s
+// testar execucao sem ser como root
+
+void showUserInfo()
+{
+    printf("uid: %d\n", getuid());
+    printf("effective uid: %d\n", geteuid());
+    printf("gid: %d\n", getgid());
+    printf("effective gid: %d\n", getegid());
+}
+
+int main(void)
+{
+    int s, first_time = 0;
+    FILE *file;
+    mode_t mode = 0700;
+    char filepath[15] = "./", filename[] = "only-root.txt", buffer[] = "hello";
+    showUserInfo();
+    strcat(filepath, filename);
+    if (access(filepath, F_OK) != 0)
+        first_time = 1;
+    if (first_time && getuid() != ROOT)
+    {
+        printf("must execute at first time as root\n");
+        exit(1);
+    }
+    file = fopen(filepath, "a");
+    if (first_time)
+        chmod(filepath, mode);
+    s = fprintf(file, "%s\n", buffer);
+    fclose(file);
+    if (s < 0)
+        exit(2);
+    exit(0);
+}
